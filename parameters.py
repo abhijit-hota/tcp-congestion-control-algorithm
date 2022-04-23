3353ce17ffb38a943e973ddf3f2faf0e122b5a10
abhijit-hota <abhihota025@gmail.com>
2022-04-23 13:55:44 +0530

    refactor & cleanup plotting function

--- parameters.py
+++ parameters.py
@@ -1,5 +1,5 @@
 from simulate import simulate
-from utils import output_file, plot_tcp
+from utils import SimulateOptions, output_file, plot_tcp
 
 
 ki_values = [1, 4]
@@ -13,7 +13,19 @@ for ps in ps_values:
         for km in km_values:
             for kn in kn_values:
                 for kf in kf_values:
-                    cw_values = simulate(ki, km, kn, kf, ps, num_segments=100000)
+                    num_segments = 100000
+                    cw_values = simulate(
+                        ki,
+                        km,
+                        kn,
+                        kf,
+                        ps,
+                        num_segments,
+                    )
                     fname = f"{ki}_{km}_{kn}_{kf}_{ps}"
                     output_file("figures/" + fname + ".log", cw_values)
-                    plot_tcp(cw_values, fname)
+                    plot_tcp(
+                        cw_values,
+                        SimulateOptions(ki, km, kn, kf, ps, num_segments),
+                        fname,
+                    )
--- utils.py
+++ utils.py
@@ -4,20 +4,45 @@ import matplotlib.pyplot as plt
 
 
 class SimulateOptions(argparse.Namespace):
-    def __init__(self):
-        self.ki: float = 1.0
-        self.km: float = 1.0
-        self.kn: float = 1.0
-        self.kf: float
-        self.ps: float
-        self.num_segments: int
-        self.output: str
+    def __init__(
+        self,
+        ki: float = 1.0,
+        km: float = 1.0,
+        kn: float = 1.0,
+        kf: float = 0.3,
+        ps: float = 0.99,
+        num_segments: int = 1000,
+        output: str = "out",
+    ):
+        self.ki: float = ki
+        self.km: float = km
+        self.kn: float = kn
+        self.kf: float = kf
+        self.ps: float = ps
+        self.num_segments: int = num_segments
+        self.output: str = output
 
 
-def plot_tcp(cw_list: List[float], name: str = "out"):
+def plot_tcp(
+    cw_list: List[float],
+    options: SimulateOptions,
+    name: str = "out",
+):
     print(len(cw_list))
     plt.plot([*range(len(cw_list))], cw_list)
-    plt.savefig(f"./figures/{name}.png")
+    plt.ylabel("Congestion Window")  # type: ignore
+    plt.xlabel("Sequence Number")  # type: ignore
+    plt.suptitle("TCP Congestion Window Simulation", fontweight="bold")
+
+    plt.title(
+        f"| $K_i$ = {options.ki} |  $K_m$ = {options.km} |  $K_n$ = {options.kn} |  $K_f$ = {options.kf} |  $P_s$ = {options.ps} |  $T$ = {options.num_segments} |",
+        fontsize=10,
+    )
+
+    plt.tight_layout(pad=0.5)
+    plt.savefig(
+        f"./figures/{name}.png",
+    )
     plt.close()
 
 
